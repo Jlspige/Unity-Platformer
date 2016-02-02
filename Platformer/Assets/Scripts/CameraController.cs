a12dec55ddcaea3e6a85c96e66f57ac071c97bb6
jlspige <j.spigelman@gmail.com>
2016-02-02 00:38:39 -0500

    This is adding the actual files for unity

---
+++ Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+	public float rotateSpeed = 0.0000000000000005f;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = target.transform.position - transform.position;
+	}
+
+	void LateUpdate()
+	{
+		float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+		float vertical = Input.GetAxis ("Mouse Y") * rotateSpeed;
+		target.transform.Rotate (0, horizontal, 0);
+
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+		transform.position = target.transform.position - (rotation * offset);
+
+		transform.LookAt(target.transform);
+	}
+}
---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public float jump;
+	public float playerGravity;
+
+	bool canJump;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		canJump = false;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		transform.Translate (speed * moveHorizontal, 0f, speed * moveVertical);
+	}
+
+	void FixedUpdate () 
+	{
+		/*float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);*/
+
+		Vector3 downF = new Vector3 (0.0f, playerGravity, 0.0f);
+		rb.AddForce(downF);
+
+		if(Input.GetKey("space") && canJump)
+		{
+			Vector3 jumpVec = new Vector3 (0.0f, jump, 0.0f);
+			rb.AddForce (jumpVec, ForceMode.Impulse);
+			canJump = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.CompareTag("platform"))
+		{
+			canJump = true;
+		}
+	}
+}
