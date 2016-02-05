a6fb794086fff2430146a9f3ba07c9c88168c407
jlspige <j.spigelman@gmail.com>
2016-02-05 01:31:10 -0500

    Changed the way movement and the camera works to work better and be easier to code for. Still needs the same additions as the last commit though.

--- Platformer/Assets/Scripts/CameraController.cs
+++ Platformer/Assets/Scripts/CameraController.cs
@@ -4,47 +4,66 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject target;
+	public Vector3 cameraOffset; //default camera distance from the player
+
+
 	public float rotateSpeed = 0.0000000000000005f;
 	private Vector3 offset;
+	private Vector3 offsetGround;
+	private Vector3 offsetAir;
 	private Vector3 newOffset;
-	public float damping = 1;
+	private Vector3 newOffsetGround;
+	private Vector3 newOffsetAir;
+	public float damping;
 
 	// Use this for initialization
 	void Start () 
 	{
+		//cam = this.GetComponent<Camera> ();
+		//cameraOffset = new Vector3 (0f, 6.4f, -18.9f);
+		//cameraOffset = new Vector3 (0f, 10.0f, -20.0f);
 		offset = target.transform.position - transform.position;
+		offsetGround = offset;
+		offsetGround.y = 0;
 	}
 
 	void LateUpdate()
 	{
-		if (Input.GetMouseButton(0)) //For manually setting the camera angle with the mouse
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+		transform.position = target.transform.position - (rotation * offset);
+		/*newOffset = target.transform.position - transform.position;
+		newOffsetGround = newOffset;
+		newOffsetGround.y = 0;
+		//If the player gets too far away from the camera, follow it
+		if(newOffsetGround.magnitude > offsetGround.magnitude)
 		{
-			/*//offset = target.transform.position - transform.position;
-			float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
-			float vertical = Input.GetAxis ("Mouse Y") * rotateSpeed;
-			target.transform.Rotate (0, horizontal, 0);
-
-			float desiredAngle = target.transform.eulerAngles.y;
-			Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
-			transform.position = target.transform.position - (rotation * offset);
-
-			transform.LookAt (target.transform);*/
-		}
-		/*else
-		{
-			float currentAngle = transform.eulerAngles.y;
-			float desiredAngle = target.transform.eulerAngles.y;
-			float angle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * damping);
-
-			Quaternion rotation = Quaternion.Euler (0, angle, 0);
-			transform.position = target.transform.position - (rotation * offset);
-
-			transform.LookAt (target.transform);
+			Vector3 newPosition = transform.position;
+			//newPosition.x += (newOffsetGround.x - offsetGround.x);
+			//newPosition.z += (newOffsetGround.z - offsetGround.z);
+			newPosition.x += newOffsetGround.x;
+			newPosition.z += newOffsetGround.z;
+			//newPosition = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 7.5f);
+			newPosition = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
+			transform.position = newPosition;
 		}*/
-		else
+		/*if(newOffsetGround.magnitude < offsetGround.magnitude * 0.5)
 		{
-			//transform.position = target.transform.position + offset;
-			transform.LookAt(target.transform);
-		}
+			Vector3 newPosition = transform.position;
+			newPosition.x -= (newOffsetGround.x - offsetGround.x);
+			newPosition.z -= (newOffsetGround.z - offsetGround.z);
+			newPosition = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 7.5f);
+			transform.position = newPosition;
+		}*/
+
+		transform.LookAt(target.transform);
+
+
+		//code for look at
+		/*Vector3 targetXZ = target.transform.position;
+		targetXZ.y = 0;
+		transform.LookAt(targetXZ);*/
+
+		//transform.rotation = Quaternion.LookRotation (newOffset, new Vector3(0, 1, 0));
 	}
 }
--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed;
+	public float speed2;
 	public float jump;
 	public float playerGravity;
 
@@ -23,24 +24,57 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3(0f, 0f, speed2 * moveVertical);
+		transform.Translate (movement);
+
+		Vector3 R = Camera.main.transform.position - transform.position;
+		Vector3 V = new Vector3(speed2 * moveHorizontal, 0f, 0f);
+		float w = 180 * V.magnitude / (Mathf.PI * R.magnitude);
+		if(moveHorizontal > 0)
+		{
+			transform.RotateAround (Camera.main.transform.position, Vector3.up, w * 1);
+		}
+		else
+		{
+			transform.RotateAround (Camera.main.transform.position, Vector3.up, w * -1);
+		}
+
+		//Code to make player face travel direction
+		//transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+		//change Vector3.forward to direction of travel after it's found using the code below
+
+
+
+
+
+
 		/*float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		transform.Translate (speed * moveHorizontal, 0f, speed * moveVertical);*/
+		Vector3 movement = new Vector3(0.5f * moveHorizontal, 0f, 0.5f * moveVertical);
+		transform.LookAt(transform.position + movement);
+		transform.Translate (movement);*/
 
-
+		/*//Code for movement relative to the camera
+		//Get the z and x unit vectors of the camera
 		Vector3 cameraForward = Camera.main.transform.forward;
 		Vector3 cameraRight = Camera.main.transform.right;
 		Vector3.Normalize (cameraForward);
 		Vector3.Normalize (cameraRight);
+		//Get the input from keyboard controls
 		float moveVertical = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
 		float moveHorizontal = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		//Combine vectors and eliminate y axis movement
 		cameraForward = cameraForward * moveVertical;
 		cameraForward.y = 0;
 		cameraRight = cameraRight * moveHorizontal;
 		cameraRight.y = 0;
-		transform.Translate(cameraForward);
-		transform.Translate (cameraRight);
-
+		//transform.Translate(cameraForward);
+		//transform.Translate (cameraRight);
+		Vector3 movement = cameraForward + cameraRight;
+		transform.Translate (movement);
+		Vector3.Normalize (movement);*/
 	}
 
 	void FixedUpdate () 
